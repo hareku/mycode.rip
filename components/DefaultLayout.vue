58035bc3dc57514b4391d1e9ed041ce215bb9184
hareku <hareku908@gmail.com>
2020-10-27 01:39:33 +0900

    Add license

--- components/DefaultLayout.vue
+++ components/DefaultLayout.vue
@@ -12,6 +12,8 @@
 
       <hr />
       <AboutMe/>
+
+      <p><a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/80x15.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</a>.</p>
       <p>This blog's source code is published on <a href="https://github.com/hareku/mycode.rip" target="_blank" rel="nofollow noreferrer">GitHub</a>.</p>
     </main>
   </div>
