b10d0c86aa76950ef3370169037842aec6c494f3
hareku <hareku908@gmail.com>
2020-10-26 22:17:23 +0900

    Fix share buttons

--- components/ShareButtons.vue
+++ components/ShareButtons.vue
@@ -3,14 +3,12 @@
     <div class="share-buttons">
       <div>
         <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" :data-text="shareText" data-related="hareku908" data-show-count="false">Tweet</a>
-        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
       </div>
 
       <div>
         <a href="http://b.hatena.ne.jp/entry/" class="hatena-bookmark-button" data-hatena-bookmark-layout="vertical-normal" data-hatena-bookmark-lang="ja" title="このエントリーをはてなブックマークに追加">
           <img src="https://b.st-hatena.com/images/v4/public/entry-button/button-only@2x.png" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" />
         </a>
-        <script type="text/javascript" src="https://b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async"></script>
       </div>
     </div>
   </client-only>
@@ -22,6 +20,23 @@ export default {
     shareText: {
       type: String,
       required: true
+    }
+  },
+
+  mounted() {
+    const links = [
+      'https://platform.twitter.com/widgets.js',
+      'https://b.st-hatena.com/js/bookmark_button.js',
+    ]
+
+    const body = document.querySelector('body')
+    if(body) {
+      links.forEach(link => {
+        const script = document.createElement('script')
+        script.setAttribute('src', link)
+        script.setAttribute('async', 'async')
+        body.appendChild(script)
+      })
     }
   }
 }
