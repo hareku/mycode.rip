40c956920b697a846c0a58ff8e52cf850525f3ca
hareku <hareku908@gmail.com>
2020-10-26 22:25:47 +0900

    Update about me

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -3,21 +3,40 @@
     <div class="profile-head">About me</div>
 
     <ul>
-      <li>
-        <a href="https://twitter.com/hareku908" target="_blank" rel="nofollow noreferrer">Twitter @hareku908</a>
-      </li>
-      <li>
-        <a href="https://github.com/hareku" target="_blank" rel="nofollow noreferrer">GitHub @hareku</a>
-      </li>
-      <li>
-        <a href="https://qiita.com/hareku" target="_blank" rel="nofollow noreferrer">Qiita @hareku</a>
+      <li v-for="link in LINKS" :key="link.url">
+        <a :href="link.url" target="_blank" rel="nofollow noreferrer">{{ link.text }}</a>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-export default {}
+const LINKS = [
+  {
+    url: 'https://twitter.com/hareku908',
+    text: 'Twitter'
+  },
+  {
+    url: 'https://github.com/hareku',
+    text: 'GitHub'
+  },
+  {
+    url: 'https://atcoder.jp/users/hareku',
+    text: 'AtCoder'
+  },
+  {
+    url: 'https://qiita.com/hareku',
+    text: 'Qiita'
+  }
+]
+
+export default {
+  data() {
+    return {
+      LINKS
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
